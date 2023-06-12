eaa07c01d462128f2d90c0d32e2355e495ce5d28
slcnx <1062670898@qq.com>
2023-06-12 16:17:26 +0800

    fix apply

--- base-docker/build-command.sh
+++ base-docker/build-command.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#docker build -t registry.cn-beijing.aliyuncs.com/cloudcenter-ops/nginx:1.22.0 -f base-Dockerfile  .
-#docker push registry.cn-beijing.aliyuncs.com/cloudcenter-ops/nginx:1.22.0
-
-crane cp nginx:1.22.0 registry.cn-beijing.aliyuncs.com/cloudcenter-ops/nginx:1.22.0
+docker build -t registry.cn-beijing.aliyuncs.com/cloudcenter-ops/nginx:1.22.0 -f base-Dockerfile  .
+docker push registry.cn-beijing.aliyuncs.com/cloudcenter-ops/nginx:1.22.0
 crane cp node:16.15.0 registry.cn-beijing.aliyuncs.com/cloudcenter-ops/node:16.15.0
